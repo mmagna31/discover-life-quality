17f2c30290e65a8a80c12068635ca63c1701969f
mmagna31 <marco.magnarelli@gmail.com>
2022-02-16 16:41:37 +0100

    test

--- src/index.js
+++ src/index.js
@@ -184,9 +184,3 @@ document.body.append(
   renderFooter(),
   renderBackToTop()
 );
-
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});

c4b4added2fee9b74d6d17a0e9478cda94a6eff2
mmagna31 <marco.magnarelli@gmail.com>
2022-02-16 16:37:26 +0100

    test

--- src/index.js
+++ src/index.js
@@ -184,3 +184,9 @@ document.body.append(
   renderFooter(),
   renderBackToTop()
 );
+
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
